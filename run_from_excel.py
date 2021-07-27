8767765ddf97b580651956586dbd084dfdfc2191
Andy Malone <amalone@hilcorp.com>
2021-07-27 18:19:21 -0500

    Intermediate Commit

--- run_from_excel.py
+++ run_from_excel.py
@@ -5,8 +5,12 @@ import Toolbox as tb
 import pandas as pd
 import openpyxl as pyx
 
-case_data = pd.read_excel('Project_Economics_Controller.xlsx', 
+case_data = pd.DataFrame(
+    pd.read_excel('Project_Economics_Controller.xlsx', 
     sheet_name = 'Data_DO_NOT_EDIT',
-    index_col = 0)
+    header = 0).T,
+    index = 0)
 
 print(case_data)
+
+print(case_data.major)
